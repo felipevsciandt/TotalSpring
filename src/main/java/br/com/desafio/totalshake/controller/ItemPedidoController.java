03161293822efbe0cc5c01f76dfff5180af4688f
Felipe Vasconcelos <felipevs@ciandt.com>
2022-08-29 17:29:48 -0300

    post itemPedido

--- src/main/java/br/com/desafio/totalshake/controller/ItemPedidoController.java
+++ src/main/java/br/com/desafio/totalshake/controller/ItemPedidoController.java
@@ -1,15 +1,16 @@
 package br.com.desafio.totalshake.controller;
 
+import br.com.desafio.totalshake.dto.ItemPedidoDto;
+import br.com.desafio.totalshake.dto.PedidoDto;
 import br.com.desafio.totalshake.model.ItemPedido;
 import br.com.desafio.totalshake.model.Pedido;
 import br.com.desafio.totalshake.service.ItemPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,8 +27,17 @@ public class ItemPedidoController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ItemPedido> findById(@PathVariable Long id) {
-        ItemPedido itemPedido = service.findById(id);
-        return ResponseEntity.ok().body(itemPedido);
+    public ResponseEntity<ItemPedidoDto> findById(@PathVariable Long id) {
+        ItemPedidoDto itemPedidoDto = service.findById(id);
+        return ResponseEntity.ok().body(itemPedidoDto);
     }
+
+    @PostMapping
+    public ResponseEntity<ItemPedido> insert(@RequestBody ItemPedidoDto itemPedidoDto) {
+        ItemPedido itemPedido = service.post(itemPedidoDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(itemPedidoDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(itemPedido);
+    }
+
 }
--- src/main/java/br/com/desafio/totalshake/service/ItemPedidoService.java
+++ src/main/java/br/com/desafio/totalshake/service/ItemPedidoService.java
@@ -1,8 +1,11 @@
 package br.com.desafio.totalshake.service;
 
+import br.com.desafio.totalshake.dto.ItemPedidoDto;
 import br.com.desafio.totalshake.model.ItemPedido;
+import br.com.desafio.totalshake.model.Pedido;
 import br.com.desafio.totalshake.repository.ItemPedidoRepository;
 import br.com.desafio.totalshake.service.exceptions.ResourceNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,8 @@ public class ItemPedidoService {
 
     @Autowired
     private ItemPedidoRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Transactional
     public List<ItemPedido> findAll() {
@@ -22,9 +27,17 @@ public class ItemPedidoService {
     }
 
     @Transactional
-    public ItemPedido findById(Long id) {
+    public ItemPedidoDto findById(Long id) {
         Optional<ItemPedido> object = repository.findById(id);
         ItemPedido itemPedido = object.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada"));
-        return itemPedido;
+        return new ItemPedidoDto(itemPedido);
+    }
+
+    @Transactional
+    public ItemPedido post(ItemPedidoDto itemPedidoDto) {
+        ItemPedido object = new ItemPedido();
+        object = repository.save(object);
+        return this.repository.save(modelMapper.map(object, ItemPedido.class));
+
     }
 }
